592e3a4ff1ee3b16e949d16e35390b2241b09498
Tomas-Wardoloff <tomaswardoloff@gmail.com>
2025-01-18 19:03:24 -0300

    Add routing to the application with Home, Login, Register, and Trip Details pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "./components/home/";
+import { LoginPage } from "./components/login";
+import { RegisterPage } from "./components/register";
+import { TripDetailsPage } from "./components/trip";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login/" element={<LoginPage />} />
+        <Route path="register/" element={<RegisterPage />} />
+        <Route path="/trip/:id" element={<TripDetailsPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
